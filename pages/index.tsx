73abead5c459630de09a5965a52526e65f59bbeb
sakurasou3 <ko.ki.0216@gmail.com>
2025-02-13 21:17:05 +0900

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="container w-full h-full mx-auto">
+      <main className="container w-full mt-16">
+        <h1 className="text-3xl text-center">Sakurasou Blog</h1>
+      </main>
+    </div>
+  );
+}
