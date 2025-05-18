e729e23a97231099c4250d62af3a8cd7fd7d7a81
sakurasou3 <ko.ki.0216@gmail.com>
2025-05-18 20:36:54 +0900

    feat: ファイル構成を修正

--- components/ui/heading.tsx
+++
@@ -1,5 +0,0 @@
-import { Heading as ChakraHeading } from "@chakra-ui/react";
-
-export const Heading = (props: ChakraHeading.RouteProps) => {
-  return <ChakraHeading {...props} />;
-};
--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactStrictMode: true,
-  experimental: {
-    optimizePackageImports: ["@chakra-ui/react"],
-  },
+    /* config options here */
 };
 
 export default nextConfig;
