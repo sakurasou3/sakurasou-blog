f3fd94ea16b261878ad71f85eaf47de3fbfa9b12
sakurasou3 <ko.ki.0216@gmail.com>
2025-07-05 16:17:03 +0900

    fix: format

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,38 +1,43 @@
-import { ColorModeButton } from "@/components/ui/color-mode";
 import { Header } from "@/components/ui/header";
 import { getAllPosts } from "@/lib/notionApi";
-import { Flex, HStack, Spacer, Stack, Tag, Text } from "@chakra-ui/react";
+import { Flex, Stack, Tag, Text, LinkBox, LinkOverlay } from "@chakra-ui/react";
 
 export default async function Home() {
-    const allPosts = await getAllPosts();
-    return (
-        <Stack padding="24px" bg="bg">
-            <Header />
+  const allPosts = await getAllPosts();
+  return (
+    <Stack padding="24px" bg="bg">
+      <Header />
 
-            {allPosts.map((data, index) => (
-                <Stack key={index.toString()} bg="bg">
-                    <Flex
-                        key={index.toString()}
-                        align="center"
-                        justifyContent="space-between"
-                        width="100%"
-                    >
-                        <Text textStyle="xl" color="fg">
-                            {data.title}
-                        </Text>
-                        <Text textStyle="xs" color="fg">
-                            {data.date}
-                        </Text>
-                    </Flex>
-                    <Flex gap={1}>
-                        {data.tags.map((tag) => (
-                            <Tag.Root key={tag} size="lg" gap={8}>
-                                <Tag.Label>{tag}</Tag.Label>
-                            </Tag.Root>
-                        ))}
-                    </Flex>
-                </Stack>
-            ))}
-        </Stack>
-    );
+      <LinkBox>
+        {allPosts.map((data, index) => (
+          <LinkOverlay
+            key={index.toString()}
+            bg="bg"
+            href={`detail/${data.slug}`}
+          >
+            <Flex
+              key={index.toString()}
+              align="center"
+              justifyContent="space-between"
+              width="100%"
+            >
+              <Text textStyle="xl" color="fg">
+                {data.title}
+              </Text>
+              <Text textStyle="xs" color="fg">
+                {data.date}
+              </Text>
+            </Flex>
+            <Flex gap={1}>
+              {data.tags.map((tag) => (
+                <Tag.Root key={tag} size="lg" gap={8}>
+                  <Tag.Label>{tag}</Tag.Label>
+                </Tag.Root>
+              ))}
+            </Flex>
+          </LinkOverlay>
+        ))}
+      </LinkBox>
+    </Stack>
+  );
 }
--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -2,12 +2,12 @@ import { HStack, Spacer, Text } from "@chakra-ui/react";
 import { ColorModeButton } from "./color-mode";
 
 export const Header = () => (
-    <HStack justifyContent="flex-end">
-        <Text textStyle="2xl" fontWeight="bold" color="fg">
-            Notion Blog
-        </Text>
-        <Spacer />
-        {/* カラーモード トグルボタン */}
-        <ColorModeButton width="30px" />
-    </HStack>
+  <HStack justifyContent="flex-end">
+    <Text textStyle="2xl" fontWeight="bold" color="fg">
+      Notion Blog
+    </Text>
+    <Spacer />
+    {/* カラーモード トグルボタン */}
+    <ColorModeButton width="30px" />
+  </HStack>
 );
