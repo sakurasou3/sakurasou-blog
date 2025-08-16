a5e546c3a4ca7ae2746b8190f483e857c426c911
sakurasou3 <ko.ki.0216@gmail.com>
2025-08-16 12:17:31 +0900

    style: Fix style

--- src/components/ui/provider.tsx
+++ src/components/ui/provider.tsx
@@ -4,9 +4,9 @@ import { ChakraProvider, defaultSystem } from "@chakra-ui/react";
 import { ColorModeProvider, type ColorModeProviderProps } from "./color-mode";
 
 export function Provider(props: ColorModeProviderProps) {
-    return (
-        <ChakraProvider value={defaultSystem}>
-            <ColorModeProvider {...props} />
-        </ChakraProvider>
-    );
+  return (
+    <ChakraProvider value={defaultSystem}>
+      <ColorModeProvider {...props} />
+    </ChakraProvider>
+  );
 }
