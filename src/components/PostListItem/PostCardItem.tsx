c8accf42605483ef890a170ed325e3775f934035
sakurasou3 <ko.ki.0216@gmail.com>
2025-08-30 10:40:22 +0900

    fix: Fix Card Layout

--- src/components/PostListItem/PostCardItem.tsx
+++ src/components/PostListItem/PostCardItem.tsx
@@ -1,5 +1,5 @@
 import { PostItem } from "@/models/post";
-import { Card, Flex, GridItem, Stack, Text } from "@chakra-ui/react";
+import { Card, Flex, Stack, Text } from "@chakra-ui/react";
 import Link from "next/link";
 import Tag from "../ui/Tag";
 
@@ -8,28 +8,25 @@ interface Props {
 }
 const PostCardItem = ({ data }: Props) => {
   return (
-    <GridItem>
+    <Card.Root variant="elevated" size="md" h="100%">
       <Link href={`detail/${data.slug}`}>
-        <Card.Root variant="elevated" size="md" h="100%">
-          <Card.Header>
-            <Card.Title>{data.title}</Card.Title>
-          </Card.Header>
-          <Card.Footer>
-            <Stack>
-              <Text textStyle="xs" color="fg">
-                {data.date}
-              </Text>
-              <Flex gap={1}>
-                {data.tags.map((tag: string) => (
-                  <Tag key={tag} tag={tag} />
-                ))}
-              </Flex>
-            </Stack>
-          </Card.Footer>
-        </Card.Root>
+        <Card.Header>
+          <Card.Title>{data.title}</Card.Title>
+        </Card.Header>
       </Link>
-    </GridItem>
+      <Card.Footer>
+        <Stack>
+          <Text textStyle="xs" color="fg">
+            {data.date}
+          </Text>
+          <Flex gap={1}>
             {data.tags.map((tag: string) => (
+              <Tag key={tag} tag={tag} />
+            ))}
+          </Flex>
+        </Stack>
+      </Card.Footer>
+    </Card.Root>
   );
 };
 
